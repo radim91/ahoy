09a9fb1129e849881bf572f94aa8e018085b6726
Radim Hejduk <dev@radimhejduk.cz>
2024-02-14 17:24:36 +0100

    projects template init

--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "radim91/entity"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+    "mime"
+	"radim91/entity"
 )
 
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("templates/projects.html")
+    
+    if err != nil {
+        panic(err)
+    }
+
+    Projects := entity.GetProjects()
+    fmt.Println(Projects)
+
+    tmpl.Execute(w, Projects)
+}
+
+func containersHandler(w http.ResponseWriter, r *http.Request) {
+    jsonData, err := json.Marshal(entity.GetContainers())
+    
+    if err != nil {
+        panic(err)
+    }
+
+    w.Write(jsonData)
+}
+
 func main() {
-    fmt.Println(entity.GetProjects())
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", projectsHandler)
+    mux.HandleFunc("/projects", projectsHandler)
+    mux.HandleFunc("/containers", containersHandler)
+
+    mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+    http.ListenAndServe(":8080", mux)
 }
